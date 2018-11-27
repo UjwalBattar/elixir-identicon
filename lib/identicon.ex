dc85f1976205417aef218392e9f1e81945658e31
Ujwal Battar <ujwal.battar@gmail.com>
2018-11-26 19:54:44 -0800

    set up identicon builder with struct

---
+++ lib/identicon.ex
@@ -0,0 +1,53 @@
+defmodule Identicon do
+  def main(input) do
+    # first function or value or item will be passed down via pipe operator
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    # Enum.filter(grid, fn(square) -> end)
+    # Enum.filter grid, fn(square) ->
+    # end
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        rem(code, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail] = row
+
+    # [145, 46, 200, 46, 145]
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+end
